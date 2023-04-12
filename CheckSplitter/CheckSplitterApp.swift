bdd216cafb45b2b34cc5ec93587a92acadcedaff
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-04-12 08:24:10 -0500

    Initial Commit

---
+++ CheckSplitter/CheckSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitterApp.swift
+//  CheckSplitter
+//
+//  Created by Jack Hyde on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
